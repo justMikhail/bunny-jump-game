3f6673a20db91e66f2775dc9cc7d2ad7f057bcee
Mikhail Sitnikov <67879032+justMikhail@users.noreply.github.com>
2022-01-27 16:13:29 +0300

    new engine for scale game

--- src/game/scripts/game.ts
+++ src/game/scripts/game.ts
@@ -13,25 +13,20 @@ import Level1Scene from './scenes/level-1-scene';
 
 type ScaleMode = 'FIT' | 'SMOOTH';
 
-const DEFAULT_WIDTH = 390;
-const DEFAULT_HEIGHT = 844;
-const MAX_WIDTH = 576;
-const MAX_HEIGHT = 1024;
-const SCALE_MODE: ScaleMode = 'SMOOTH'; // FIT OR SMOOTH
+const gameScreenInfo = document.getElementById('game-screen').getBoundingClientRect();
+const currentGameScreenWidth = gameScreenInfo.width;
+const currentGameScreenHeight = gameScreenInfo.height;
 
 const initGame = (gameContainer, gameWidth: number, gameHeight: number) => {
   const mainGameConfig: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    width: gameWidth,
-    height: gameHeight,
     backgroundColor: GameColor.MainBackgroundColor,
     parent: gameContainer,
     scale: {
-      mode: Phaser.Scale.NONE,
-      width: DEFAULT_WIDTH,
-      height: DEFAULT_HEIGHT,
-      // mode: Phaser.Scale.FIT,
-      // autoCenter: Phaser.Scale.CENTER_BOTH,
+      width: gameWidth,
+      height: gameHeight,
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
     },
     physics: {
       default: 'arcade',
@@ -56,51 +51,5 @@ const initGame = (gameContainer, gameWidth: number, gameHeight: number) => {
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-  const game = initGame(DataBase.GameContainerId, window.innerWidth, window.innerHeight);
-
-  const resize = () => {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-
-    const width = DEFAULT_WIDTH;
-    const height = DEFAULT_HEIGHT;
-    const maxWidth = MAX_WIDTH;
-    const maxHeight = MAX_HEIGHT;
-    const scaleMode = SCALE_MODE;
-
-    const scale = Math.min(w / width, h / height);
-    const newWidth = Math.min(w / scale, maxWidth);
-    const newHeight = Math.min(h / scale, maxHeight);
-
-    const defaultRatio = DEFAULT_WIDTH / DEFAULT_HEIGHT;
-    const maxRatioWidth = MAX_WIDTH / DEFAULT_HEIGHT;
-    const maxRatioHeight = DEFAULT_WIDTH / MAX_HEIGHT;
-
-    // smooth scaling
-    let smooth = 1;
-    if (scaleMode === 'SMOOTH') {
-      const maxSmoothScale = 1.15;
-      const normalize = (value: number, min: number, max: number) => (value - min) / (max - min);
-      if (width / height < w / h) {
-        smooth = -normalize(newWidth / newHeight, defaultRatio, maxRatioWidth) / (1 / (maxSmoothScale - 1)) + maxSmoothScale;
-      } else {
-        smooth = -normalize(newWidth / newHeight, defaultRatio, maxRatioHeight) / (1 / (maxSmoothScale - 1)) + maxSmoothScale;
-      }
-    }
-
-    // resize the game
-    game.scale.resize(newWidth * smooth, newHeight * smooth);
-
-    // scale the width and height of the css
-    game.canvas.style.width = `${newWidth * scale}px`;
-    game.canvas.style.height = `${newHeight * scale}px`;
-
-    // center the game with css margin
-    // game.canvas.style.marginTop = `${(h - newHeight * scale) / 2}px`;
-    // game.canvas.style.marginLeft = `${(w - newWidth * scale) / 2}px`;
-  };
-  window.addEventListener('resize', (event) => {
-    resize();
-  });
-  resize();
+  initGame(DataBase.GameContainerId, currentGameScreenWidth, currentGameScreenHeight);
 });
