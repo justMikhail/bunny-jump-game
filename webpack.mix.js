d44520038daabde94bc0cd3b6e9a9ae5587b2cca
Mikhail Sitnikov <mikhail.walle@gmail.com>
2022-01-30 17:40:12 +0300

    add "material-theme-creator" to project

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,36 +2,39 @@ const mix = require('laravel-mix');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
-mix.webpackConfig({
-  plugins: [
-    // Перед каждым новым запуском папка /dist будет очищена
-    new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: ['dist'],
-    }),
-    new ESLintPlugin({
-      fix: true,
-    }),
-  ],
-});
+mix
+  .webpackConfig({
+    plugins: [
+      new CleanWebpackPlugin({
+        cleanAfterEveryBuildPatterns: ['dist'],
+      }),
+      new ESLintPlugin({
+        fix: true,
+      }),
+    ],
+  });
 
-mix.options({
-  processCssUrls: false,
-});
+mix
+  .options({
+    processCssUrls: false,
+  });
 
-mix.js('src/ui/js/app.js', 'js')
+mix
+  .js('src/ui/js/app.js', 'js')
   .ts('src/game/scripts/game.ts', 'js')
   .sass('src/ui/styles/app.scss', 'css')
   .sourceMaps()
-  .copyDirectory('src/ui/fonts', 'dist/fonts')
+  // .copyDirectory('src/ui/fonts', 'dist/fonts')
   .copyDirectory('src/ui/img', 'dist/img')
   .copyDirectory('src/game/assets/sprites', 'dist/img/sprites')
   .copyDirectory('src/game/assets/sounds', 'dist/sounds')
   .copyDirectory('src/ui/*.html', 'dist')
   .setPublicPath('dist');
 
-mix.browserSync({
-  watch: true,
-  ghostMode: false,
-  notify: false,
-  server: './dist',
-});
+mix
+  .browserSync({
+    watch: true,
+    ghostMode: false,
+    notify: false,
+    server: './dist',
+  });
