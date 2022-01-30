0258be7ec869e2c0150659e4fe019ba74d3e4708
Mikhail Sitnikov <mikhail.walle@gmail.com>
2022-01-30 19:42:04 +0300

    init "swiper" (base config)

--- src/ui/js/app.js
+++ src/ui/js/app.js
@@ -1,4 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-  // code...
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 });
